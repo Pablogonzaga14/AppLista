ef14cd51e87b09493cb26d399191c2d6ae6315c8
Pablogonzaga14 <pablogonzaga14@gmail.com>
2023-03-24 09:35:17 -0300

    App lista curso vip teste primeiro comit

--- app/src/main/java/devandroid/vk/applistacurso/MainActivity.java
+++ app/src/main/java/devandroid/vk/applistacurso/MainActivity.java
@@ -5,8 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
+    //teste pra essa porra
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
