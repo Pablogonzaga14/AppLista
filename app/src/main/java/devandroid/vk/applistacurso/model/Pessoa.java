5ee404d4e2618e956ae0dbd19f674888cf3d8378
Pablogonzaga14 <pablogonzaga14@gmail.com>
2023-04-26 21:35:39 -0300

    Herança para poo

--- app/src/main/java/devandroid/vk/applistacurso/model/Pessoa.java
+++ app/src/main/java/devandroid/vk/applistacurso/model/Pessoa.java
@@ -1,10 +1,61 @@
 package devandroid.vk.applistacurso.model;
 
+
 public class Pessoa {
 
     //Construtor  para criar o objeto
+    // Atributos - Objetos - Modelo -  Templete
+    // Metodos de Acessos - Gets and Sets
 
-    public Pessoa(){
+    private String primeiroNome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefoneContato;
+    public Pessoa(){}
 
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
+    // To String - para textos - para Strings vazias
+
+
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "primeiroNome='" + primeiroNome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", cursoDesejado='" + cursoDesejado + '\'' +
+                ", telefoneContato='" + telefoneContato + '\'' +
+                '}';
     }
 }
--- app/src/main/java/devandroid/vk/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/vk/applistacurso/view/MainActivity.java
@@ -3,6 +3,7 @@ package devandroid.vk.applistacurso.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import devandroid.vk.applistacurso.R;
 import devandroid.vk.applistacurso.model.Pessoa;
@@ -10,6 +11,10 @@ import devandroid.vk.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
     @Override
     //teste pra essa porra
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        // Atribuir contrudo, dados, valores ao objeto.  Conforme o modelo, Template
+        pessoa.setPrimeiroNome("Pablo");
+        pessoa.setSobrenome("Gonzaga");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("21965329540");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Luiz");
+        outraPessoa.setSobrenome("Alvez");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("11998877445");
+
+        dadosPessoa = "Primeiro Nome ";
+        dadosPessoa +=  pessoa.getPrimeiroNome();
+        dadosPessoa += "Sobrenome ";
+        dadosPessoa +=  pessoa.getSobrenome();
+        dadosPessoa += "Curso desejado ";
+        dadosPessoa +=  pessoa.getCursoDesejado();
+        dadosPessoa += "Telefone de contato ";
+        dadosPessoa +=  pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro Nome ";
+        dadosOutraPessoa +=  outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += "Sobrenome ";
+        dadosOutraPessoa +=  outraPessoa.getSobrenome();
+        dadosOutraPessoa += "Curso desejado ";
+        dadosOutraPessoa +=  outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += "Telefone de contato ";
+        dadosOutraPessoa +=  outraPessoa.getTelefoneContato();
+
+        Log.i("POOAndroid","Objeto pessoa: "+pessoa.toString());
+        Log.i("POOAndroid", "Objeto outraPessoa: "+outraPessoa.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
